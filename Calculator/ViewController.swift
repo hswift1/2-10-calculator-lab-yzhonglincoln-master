494a16bc0cff7636bf8f20d5997e9d23f7974ed7
Soft Student <softdevstudent@robotics-mba-02.local>
2019-02-12 08:11:45 -0500

    test

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+// test from Ms. Swift
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
